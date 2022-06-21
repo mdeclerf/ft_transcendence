f3e26946dbd25befb9c44daa22e98d5037df3d46
Marie Declerfayt <mdeclerf@fu-r6-p5.s19.be>
2022-06-21 13:49:07 +0200

    mmmmh

--- here/src/App.tsx
+++ here/src/App.tsx
@@ -2,38 +2,47 @@ import React, {useRef, useEffect} from 'react';
 import './App.css';
 
 interface ball {
-	ballX: number;
-	ballY: number;
-	ballSpeedX: number;
-	ballSpeedY: number;
+	X: number;
+	Y: number;
+	SpeedX: number;
+	SpeedY: number;
 }
 
 interface paddle {
-	paddleX: number;
-	paddleY: number;
-	paddleWidth: number;
-	paddleHeight: number;
+	X: number;
+	Y: number;
+	Width: number;
+	Height: number;
 }
 
 interface score {
 	player1: number;
 	player2: number;
+	winningscore: number;
+	haswon: boolean;
+}
+
+interface dimcanvas {
+	width: number;
+	height: number;
 }
 
 const App: React.FC<{}> = () => {
 	let canvasRef = useRef<HTMLCanvasElement | null>(null);
 	let canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
 
-	let paddle1: paddle = { paddleX: 10, paddleY: 250, paddleWidth: 15, paddleHeight: 100};
-	let paddle2: paddle = { paddleX: 580, paddleY: 250, paddleWidth: 15, paddleHeight: 100};
-	let ball: ball = { ballX: 50, ballY: 50, ballSpeedX: 10, ballSpeedY: 4};
+	let dim: dimcanvas = { width: 800, height: 600};
+	let paddle1: paddle = { X: 10, Y: dim.height / 2 - 50, Width: 15, Height: 100};
+	let paddle2: paddle = { X: dim.width - 25, Y: dim.height / 2 - 50 , Width: 15, Height: 100};
+	let score: score = { player1: 0, player2: 0, winningscore: 3, haswon: false};
+	let ball: ball = { X: dim.width / 2, Y: dim.height/2, SpeedX: 10, SpeedY: 4};
 
 	const makeRectangleShape = (ctx: CanvasRenderingContext2D | null, cX: number, cY:number, width:number, height:number, color:string ) =>
 	{
 		if(ctx)
 		{
 			ctx.fillStyle = color; 
-			ctx.fillRect( cX, cY,width, height);
+			ctx.fillRect( cX, cY, width, height);
 		}
 	}
 
@@ -48,23 +57,109 @@ const App: React.FC<{}> = () => {
 		}
 	}
 
+	const makescores = (ctx: CanvasRenderingContext2D | null, score :score, dim:dimcanvas ) => {
+		if(ctx)
+		{
+			ctx.font = '25px serif';
+			ctx.fillStyle = 'black';
+			ctx.fillText(("Score: " + score.player1 ), 100, 30);
+			ctx.fillText(("Score: " + score.player2 ), dim.width - 150, 30);
+		}
+	}
+
+	const computerAi = (paddle2: paddle, ball: ball) => {
+		let paddle2YCenter: number = paddle2.Y + (paddle2.Height / 2) ;
+		if(paddle2YCenter < ball.Y - 40) { paddle2.Y += 13 }
+		 else if(paddle2YCenter < ball.Y + 40) { paddle2.Y -= 13 }
+	}
+
+	const move = (ctx: CanvasRenderingContext2D | null, score :score, paddle1:paddle, paddle2:paddle, ball:ball, dim:dimcanvas) => {
+		if(score.haswon)
+		{
+			if(score.player1 >= score.winningscore && ctx)
+			{
+				ctx.font = '25px serif';
+				ctx.fillStyle = 'black';
+				ctx.fillText("Player 1 wins", dim.height / 4 , 50);
+			}
+			if(score.player2 >= score.winningscore && ctx)
+			{
+				ctx.font = '25px serif';
+				ctx.fillStyle = 'black';
+				ctx.fillText("Player 2 wins", dim.height / 4 , 50);
+			}
+			return;
+		}
+
+		computerAi(paddle2, ball);
+
+		ball.X += ball.SpeedX;
+		ball.Y += ball.SpeedY;
+
+		if(ball.X > dim.height) { //Bouncing the ball in the X Axix      ballSpeedX = -ballSpeedX
+			if( ball.Y > paddle2.Y && ball.Y < (paddle2.Height + paddle2.Y))
+			{
+				ball.SpeedX = - ball.SpeedX; 
+				let deltaY = ball.Y - (paddle2.Y + paddle2.Height / 2);
+				ball.SpeedY = deltaY * 0.35;
+			}
+			else
+			{
+				score.player1 += 1;
+				ballReset(score, paddle1, paddle2, ball, dim);
+			}
+		}
+
+		if(ball.Y > 590) { ball.SpeedY = -ball.SpeedY }  //Bouncing the ball in the Y Axix
+		if(ball.Y < 10) { ball.SpeedY = -ball.SpeedY }
+	}
+
+	const ballReset = (score :score, paddle1:paddle, paddle2:paddle, ball:ball, dim:dimcanvas) => {
+		if (score.player1 >= score.winningscore || score.player2 >= score.winningscore ) { score.haswon = true; }
+
+		ball.SpeedX =- ball.SpeedX;
+		ball.X = dim.width / 2;
+		ball.Y = dim.height / 2;
+	}
+
+	const calculateMousePosition = (canvasRef : HTMLCanvasElement | null, ctx: CanvasRenderingContext2D, e: React.MouseEvent<HTMLButtonElement>) => {
+		if(canvasRef)
+		{
+			let rect = canvasRef.getBoundingClientRect();
+			let root = document.documentElement;
+			let mouseX = e.clientX - rect.left - root.scrollLeft;
+			let mouseY = e.clientY - rect.top - root.scrollTop;
+			return { x: mouseX, y: mouseY }
+		}
+	}
+
+	const handleMouseClick = (evt:any, score:score) => {
+        if(score.haswon){
+          score.player1 = 0;
+          score.player2 = 0;
+          score.haswon = false;
+        }
+      }
+
 	useEffect(() => {
 		// Initialize
 		if (canvasRef.current) {
 			canvasCtxRef.current = canvasRef.current.getContext('2d');
 			let ctx = canvasCtxRef.current;
-			makeRectangleShape(ctx,  paddle1.paddleX,  paddle1.paddleY, paddle1.paddleWidth, paddle1.paddleHeight, 'blue' );
-			makeRectangleShape(ctx,  paddle2.paddleX,  paddle1.paddleY, paddle2.paddleWidth, paddle2.paddleHeight, 'blue' );
-			makeCircleShape(ctx, ball.ballX, ball.ballY, 10, 0, 'yellow')
+			makeRectangleShape(ctx,  paddle1.X,  paddle1.Y, paddle1.Width, paddle1.Height, 'blue' );
+			makeRectangleShape(ctx,  paddle2.X,  paddle1.Y, paddle2.Width, paddle2.Height, 'blue' );
+			makeCircleShape(ctx, ball.X, ball.Y, 10, 0, 'yellow');
+			makescores(ctx, score, dim);
 		}
 		}, []);
 
 	return <canvas
+	id = 'canvas'
 	style={{border : "1px solid #000"}}
-	width={600}
+	width={800}
 	height={600}
 	ref={canvasRef}>
-	</canvas>;
+	</canvas>
 };
 
 export default App;
