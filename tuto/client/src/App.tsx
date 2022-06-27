ad7334a559ffc0561e1add9e8a292a5b0f477a1b
Marie Declerfayt <mdeclerf@mi-r4-p6.s19.be>
2022-06-27 16:58:44 +0200

    test

---
+++ tuto/client/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+import Canvas from './components/canvas';
+
+function App() {
+	return (
+    <div className="App">
+    <Canvas/>
+    </div>
+  );
+}
+
+export default App;
--- tuto/server/src/app.gateway.ts
+++ tuto/server/src/app.gateway.ts
@@ -3,14 +3,15 @@ import { SubscribeMessage,
 	WebSocketGateway, 
 	OnGatewayInit, 
 	OnGatewayConnection, 
-	OnGatewayDisconnect } from '@nestjs/websockets';
+	OnGatewayDisconnect,
+	WebSocketServer } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io'
 // npm i --save @nestjs/websockets @nestjs/platform-socket.io
 
 @WebSocketGateway()
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
-	// @WebSocketServer() wss: Server; // if were to send the msg to everyone
+	@WebSocketServer() wss: Server; // if were to send the msg to everyone
 
 	private logger: Logger = new Logger('AppGateway');
 	
@@ -31,13 +32,15 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
 //     return { event: 'msgToClient', data: 'Hello World'}; // only the client that sent the msg gets the reply
 //   }
 
-	@SubscribeMessage('msgToServer')
-	handleMessage(client: Socket, text: string): void { 
-		client.emit('msgToClient', text); // take text sent by client and emit an event pass text back
-	}
+	// @SubscribeMessage('msgToServer')
+	// handleMessage(client: Socket, text: string): void { 
+	// 	client.emit('msgToClient', text); // take text sent by client and emit an event pass text back
+	// }
 
-//     @SubscribeMessage('msgToServer') // if were to send this to everyone
-//   handleMessage(client: Socket, text: string): void { 
-//     this.wss.emit('msgToClient', text);
-//   }
+	@SubscribeMessage('msgToServer') // if were to send this to everyone
+	handleMessage(client: Socket, text: string): void {
+	this.wss.emit('msgToClient', text);
+	console.log(`client: `, client)
+	console.log(`text: `, text)
+	}
 }
