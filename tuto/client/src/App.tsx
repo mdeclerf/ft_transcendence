b05dae861748d656666e408a52565cdf6d4b7f09
Marie Declerfayt <mdeclerf@fu-r6-p5.s19.be>
2022-06-29 17:37:06 +0200

    wip

--- tuto/client/src/App.tsx
+++ tuto/client/src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { useEffect, useState, useRef } from 'react';
 import './App.css';
-import socketio, { Socket, io } from "socket.io-client";
-import { syncBuiltinESMExports } from 'module';
+import { Socket, io } from "socket.io-client";
 
 const H: number = 100;
 const W: number = 15;
 const paddX: number = 100;
-let first: boolean = true;
 
 let socket: Socket = io('http://localhost:3001/');
 
-function App() {
+const App = () => {
 	
 	let canvas: HTMLCanvasElement;
 	let ctx: any;
@@ -26,63 +24,46 @@ function App() {
 
 	const test1 = (e: any) => {
 		if (e.code === "ArrowUp") {
-			if (hint != "up")
-			{
-				socket.emit("send_message", "move_up");
-				console.log("up");
-				hint = "up";
-			}
+			socket.emit("send_message", "move_up");
+			console.log("up");
+			hint = "up";
 		}
 		else if (e.code === "ArrowDown") {
-			if (hint != "down")
-			{
-				socket.emit("send_message", "move_down");
-				console.log("up");
-				hint = "down";
-			}
+			socket.emit("send_message", "move_down");
+			console.log("up");
+			hint = "down";
 		}
 	}
 
 	const test2 = (e: any) => {
 		if (e.code === "ArrowDown" || e.code === "ArrowUp") {
-			if (hint != "stop")
-			{
-				socket.emit("send_message", "stop");
-				console.log("stop");
-				hint = "stop";
-			}
+			socket.emit("send_message", "stop");
+			console.log("stop");
+			hint = "stop";
 		}
 	}
 		
 	useEffect(() => {
-		if(first) {
-
-			first = false;
-			return;
-		}
+		console.log("entered");
 
 		if(canvasRef.current) {
 			canvas = canvasRef.current;
 			ctx = canvas.getContext('2d');
 		};
 
-		socket.on("receive_message", (data) => {
-			draw(data.message);
-		});
+		// socket.on("receive_message", (data) => {
+		// 	draw(data.message);
+		// });
 
 		window.addEventListener('keydown', test1);
 
 		window.addEventListener('keyup', test2);
 
 		return () => {
-			socket.close();
-
 			window.removeEventListener('keydown', test1);
-
 			window.removeEventListener('keyup', test2);
-		}
-
-	}, []);
+		};
+	},[]);
 
 	return (<canvas ref = {canvasRef} width = "1000" height = "600" />);
 };
--- tuto/server/src/app.gateway.ts
+++ tuto/server/src/app.gateway.ts
@@ -15,32 +15,55 @@ import { Socket, Server } from 'socket.io'
 	},
 })
 
-export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
-	@WebSocketServer() server: Server; // if were to send the msg to everyone
-
+	@WebSocketServer() server;
 	private logger: Logger = new Logger('AppGateway');
+  users: number = 0;
+  async handleConnection() {
+
+    // A client has connected
+    this.users++;
+    // Notify connected clients of current users
+    this.server.emit('users', this.users);
+  }
+  async handleDisconnect() {
+    // A client has disconnected
+    this.users--;
+    // Notify connected clients of current users
+    this.server.emit('users', this.users);
+  }
+  @SubscribeMessage("send_message")
+  async onChat(client, message) {
+	this.logger.log("test");
+    client.broadcast.emit("send_message", message);
+  }
+
+	// @WebSocketServer() server: Server; // if were to send the msg to everyone
+
+	// private logger: Logger = new Logger('AppGateway');
 	
-	afterInit(server: Server) { // will run as soo nas the gateway initializes
-		this.logger.log("initialized"); // upon initialization, this runs
-	}
+	// afterInit(server: Server) { // will run as soo nas the gateway initializes
+	// 	this.logger.log("initialized"); // upon initialization, this runs
+	// }
 	
-	handleConnection(client: Socket, ...args: any[]){
-		this.logger.log(`client connected: ${client.id}`); // upon an client connection this runs with unique client id
-		this.server.on("connection", (socket) => {
+	// handleConnection(client: Socket, ...args: any[]){
+	// 	this.logger.log(`client connected: ${client.id}`); // upon an client connection this runs with unique client id
+	// 	this.server.on("connection", (socket) => {
 
-			socket.on("send_message", (data) => {
-				socket.emit("receive_message", data);
-				this.logger.log(`message emited : ${data}`);
+	// 		socket.on("send_message", (data) => {
+	// 			socket.emit("receive_message", data);
+	// 			this.logger.log(`message emited : ${data}`);
+	// 			socket.disconnect();
+	// 			// socket.to(data.room).emit("receive_message", data);
+	// 		});
+	// 	});
+	// }
 
-				// socket.to(data.room).emit("receive_message", data);
-			});
-		});
-	}
-
-	handleDisconnect(client: Socket){
-		this.logger.log(`client disconnected: ${client.id}`); // upon disconnection this runs with unique client id
-	}
+	// handleDisconnect(client: Socket){
+	// 	client.disconnect();
+	// 	this.logger.log(`client disconnected: ${client.id}`); // upon disconnection this runs with unique client id
+	// }
 
 	// https://socket.io/docs/v4/broadcasting-events/
 	// io.emit("hello", "world"); to all connected clients
@@ -52,13 +75,50 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
 	// https://socket.io/docs/v4/listening-to-events/
 	// socket.on(eventName, listener)
 
-
-	// @SubscribeMessage('update_dy') // if were to send this to everyone
-	// update_y(client: Socket, text: string): void {
-	// this.server.emit('new_dy', text);
+	// @SubscribeMessage("send_message") // if were to send this to everyone
+	// send_message(client: Socket, text: string): void {
+	// this.server.emit("receive_message", text);
 	// // client.emit('updated_dy', text);
 	// console.log(`client: `, client)
 	// console.log(`text: `, text)
 	// }
-
 }
+
+// import {
+// 	WebSocketGateway,
+// 	WebSocketServer,
+// 	SubscribeMessage,
+// 	OnGatewayConnection,
+// 	OnGatewayDisconnect,
+// 	OnGatewayInit
+//   } from '@nestjs/websockets';
+//   import { Socket, Server } from 'socket.io'
+
+
+//   @WebSocketGateway({
+// 	cors: {
+// 	  origin: "http://localhost:3000",
+// 	  methods: ["GET", "POST"],
+// 	},
+// })
+
+// export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
+// 	@WebSocketServer() server;
+// 	users: number = 0;
+// 	async handleConnection() {
+// 	  // A client has connected
+// 	  this.users++;
+// 	  // Notify connected clients of current users
+// 	  this.server.emit('users', this.users);
+// 	}
+// 	async handleDisconnect() {
+// 	  // A client has disconnected
+// 	  this.users--;
+// 	  // Notify connected clients of current users
+// 	  this.server.emit('users', this.users);
+// 	}
+// 	@SubscribeMessage('chat')
+// 	async onChat(client, message) {
+// 	  client.broadcast.emit('chat', message);
+// 	}
+// }
