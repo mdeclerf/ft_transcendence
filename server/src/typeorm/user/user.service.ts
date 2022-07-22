3598fa84993ad763d6aff49ac29893b168300250
viforget <victor.forget99@gmail.com>
2022-07-22 14:36:43 +0200

    Add getUserById and getUsers functions

--- server/src/typeorm/user/user.service.ts
+++ server/src/typeorm/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+	@InjectRepository(User)
+	private readonly repository: Repository<User>
+
+	public getUserById(id: number): Promise<User>
+	{
+		return this.repository.findOneBy({id: id});
+	}
+
+	public getUsers(): Promise<User[]>
+	{
+		return this.repository.find({order: {id: 'ASC'}})
+	}
+}
